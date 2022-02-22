7bc136ad48f7059b6ca2fe784d9534bebd7d8677
Bishoy <bishoy.samwuel@gmail.com>
2022-02-22 14:02:17 +0200

    Preparing connection to mongoose

--- server/index.js
+++ server/index.js
@@ -1,5 +1,22 @@
-import { Express } from "express";
+import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors"
 
+const app = express();
+
+//prepare bodyParser to send the request
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+//connect server app with DB 
+const CONNECTION_URL = 'mongodb+srv://bishoy:bishoy@cluster0.0wy54.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
+
+const PORT = process.env.PORT || 3000;
+
+mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
+  .then(()=> app.listen(PORT, ()=>console.log(`Server running on port: ${PORT}`)))
+  .catch((error)=> console.log(error.message));
+
+// mongoose.set('useFindAndModify', false);
