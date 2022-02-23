a2cc6a3f05e8fb82610fd945c8f2f9b4ac606f0a
Bishoy <bishoy.samwuel@gmail.com>
2022-02-23 17:37:28 +0200

    Add styles to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,30 @@
 import React from "react";
-import {Container, AppBar, Typography, Grow, Grid } from '@material-ui/core';
-import memories from './images/memories.jpeg'
+import { Container, AppBar, Typography, Grow, Grid, Box } from '@mui/material';
+import memories from './images/memories.png'
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
+import styles from './styles';
 
-const App =() => {
+const App = () => {
   return (
     <Container maxidth="lg">
-      <AppBar position="static" color="inherit">
-        <Typography variant="h2" align="center">
-        Memories
+      <AppBar sx={styles.appBar} position="static" color="inherit">
+        <Typography sx={styles.heading} variant="h2" align="center">
+          Memories
         </Typography>
-          <img src={memories} alt = "memories" height="auto"/>
+        {/* <Image sx={styles.image} src={memories} alt="memories" height={60} /> */}
+        <Box component="img" sx={styles.image} alt="memories"  src={memories} />
+        {/* <img  src={memories} alt="memories" height="60" /> */}
       </AppBar>
       <Grow in>
         <Container>
-          <Grid justify="space-between" alignItems="stretch" spacing={3}>
-            <Grid item xs={12} sm={7}>
+          <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+            <Grid item xs={12} sm={7} >
               <Posts />
             </Grid>
-            <Grid item xs={12} sm={7}>
+            <Grid item xs={12} sm={4}>
               <Form />
             </Grid>
-            <Grid item xs={12} sm={7}></Grid>
-
           </Grid>
         </Container>
       </Grow>
