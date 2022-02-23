7997be24e75943be51875883e955af277fb34c6f
Bishoy <bishoy.samwuel@gmail.com>
2022-02-23 11:53:57 +0200

    Change the port

--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use(cors());
 //connect server app with DB 
 const CONNECTION_URL = 'mongodb+srv://bishoy:bishoy@cluster0.0wy54.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(()=> app.listen(PORT, ()=>console.log(`Server running on port: ${PORT}`)))
