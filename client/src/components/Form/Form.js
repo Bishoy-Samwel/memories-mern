08722f41ebbf92814060687cf63af1feb5894ec1
Bishoy <bishoy.samwuel@gmail.com>
2022-02-24 17:12:55 +0200

    Add Form design structure and style

--- client/src/components/Form/Form.js
+++ client/src/components/Form/Form.js
@@ -1,10 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { TextField, Button, Typography, Paper } from '@mui/material'
+import styles from './styles';
+import FileBase from 'react-file-base64'
+
+const classes = styles();
 
 const Form = () => {
+  const [postData, setPostData] = useState({
+    creator: '', title: '', message: '', tags: '', selectedFIles: ''
+  });
+  const handleSubmit = () => {
+
+  }
+  const clear = () => {
+
+  }
   return (
-    <h1>
-      Form
-    </h1>
+    <Paper sx={classes.paper}>
+      <form autoComplete='off' noValidate sx={classes.form} onSubmit={handleSubmit}>
+        <Typography variant="h6"> Creating a Memory</Typography>
+        <TextField name="creator" variant="outlined" label="Creator" fullWidth value={postData.creator} onChange={(e) => setPostData({ ...postData, creator: e.target.value })} />
+        <TextField name="title" variant="outlined" label="Title" fullWidth value={postData.creator} onChange={(e)=> setPostData({...postData, title: e.target.value}) } />
+        <TextField name="message" variant="outlined" label="Message" fullWidth value={postData.creator} onChange={(e)=> setPostData({...postData, message: e.target.value}) } />
+        <TextField name="tags" variant="outlined" label="Tags" fullWidth value={postData.creator} onChange={(e)=> setPostData({...postData, tags: e.target.value}) } />
+        <div style={classes.fileInput}>
+        <FileBase type="file" multiple={false} onDone={(base64)=> setPostData({...postData, selectedFile: base64})} /> 
+        </div>
+        <Button sx={classes.buttonSubmit} variant="contained" color="primary" size="large" type="submit" fullWidth>Submit</Button>
+        <Button sx={classes.buttonSubmit} variant="contained" color="secondary" size="small" onClick={clear} fullWidth>Clear</Button>
+      </form>
+    </Paper>
   );
 }
 
