f341e0d8b696b7d3ed174e363d1de2de8043defe
Bishoy <bishoy.samwuel@gmail.com>
2022-02-24 17:16:39 +0200

    Add CreatePost thunk

--- client/src/Redux/posts.js
+++ client/src/Redux/posts.js
@@ -1,4 +1,4 @@
-import * as api from '../api'
+import * as api from '../api/api'
 
 //Action Types
 const FETCH_ALL = 'FETCH_ALL'
@@ -28,6 +28,18 @@ export const getPosts = () => async (dispatch) => {
 
   try {
     const { data } = await api.fetchPosts();
+    action.payload = data;
+    dispatch(action);
+  } catch (error) {
+    console.log(error.message);
+  }
+}
+
+export const createPost = (post) => async (dispatch) => {
+  const action = { type: CREATE, payload: [] }
+
+  try {
+    const { data } = await api.createPost();
     action.payload = data;
     dispatch(action);
   } catch (error) {
