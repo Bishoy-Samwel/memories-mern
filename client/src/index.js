804d631c76db331a6d4227e5c69d8b396faad8b9
Bishoy <bishoy.samwuel@gmail.com>
2022-02-25 08:16:40 +0200

    Changing the port 🙌🏻

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,10 @@
 import React from 'react';
-
 import ReactDOM from 'react-dom';
-
 import { Provider } from 'react-redux'
-
 import { createStore, applyMiddleware, compose } from 'redux';
-
 import thunk from 'redux-thunk'
-
-import App from './App';
-
 import reducers from './Redux/reducers';
+import App from './App';
 
 const store = createStore(reducers, compose(applyMiddleware(thunk)))
 
--- server/index.js
+++ server/index.js
@@ -16,7 +16,7 @@ app.use('/posts', postRoutes) //setting the starting path (prefix)
 //connect server app with DB 
 const CONNECTION_URL = 'mongodb+srv://bishoy:bishoy@cluster0.0wy54.mongodb.net/myFirstDatabase?retryWrites=true&w=majority';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 
 mongoose.connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true})
   .then(()=> app.listen(PORT, ()=>console.log(`Server running on port: ${PORT}`)))
