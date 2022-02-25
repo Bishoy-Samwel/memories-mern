9c26c85bcb230611ed7311cb730fec126efa77a2
Bishoy <bishoy.samwuel@gmail.com>
2022-02-25 16:57:58 +0200

    Add updatePost action inside controller

--- server/controllers/postsController.js
+++ server/controllers/postsController.js
@@ -1,24 +1,34 @@
-import PostMessage from "../models/postMessage.js";
+import PostModel from "../models/postModel.js";
 
 export const getPosts = async (req, res) => {
   try {
-    const postMessages = await PostMessage.find();
+    const postMessages = await PostModel.find();
 
     res.status(200).json(postMessages);
 
   } catch (error) {
-    res.status(404).json({ message: error.message})
+    res.status(404).json({ message: error.message })
   }
 }
 
 export const createPost = async (req, res) => {
   const post = req.body;
 
-  const newPost = new PostMessage(post);
+  const newPost = new PostModel(post);
   try {
-    await  newPost.save();
+    await newPost.save();
     res.status(201).json(newPost);
   } catch (error) {
-    res.status(409).json({message: error.message});
+    res.status(409).json({ message: error.message });
   }
+}
+
+export const updatePost = async (req, res) => {
+  const { id: _id } = req.params;
+  const post = req.body;
+  if (!mongoose.Types.ObjectId.isValid(_id)) return res.status(404).send('No post with that id');
+
+  const updatePost = await PostModel.findByIdAndUpdate(_id, post, { new: true })
+
+  res.json(updatePost)
 }
