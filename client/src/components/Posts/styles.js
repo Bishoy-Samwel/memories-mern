cf5c711f173663e0b867e522645ca455a32c2125
Bishoy <bishoy.samwuel@gmail.com>
2022-02-25 12:48:10 +0200

    Add styles to posts

---
+++ client/src/components/Posts/styles.js
@@ -0,0 +1,18 @@
+
+const styles = (theme) => {
+
+  return ({
+    mainContainer: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    smMargin: {
+      margin: theme.spacing(1),
+    },
+    actionDiv: {
+      textAlign: 'center',
+    },
+  })
+}
+
+export default styles;
