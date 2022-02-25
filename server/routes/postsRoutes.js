0aee78c0eebb570cc113b1e0ed93dda4f4f6b3eb
Bishoy <bishoy.samwuel@gmail.com>
2022-02-25 17:12:08 +0200

    Fix import

--- server/routes/postsRoutes.js
+++ server/routes/postsRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getPosts, createPost } from '../controllers/postsController.js';
+import { getPosts, createPost, updatePost } from '../controllers/postsController.js';
 
 const router = express.Router();
 
