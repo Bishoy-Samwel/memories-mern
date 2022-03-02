4fbe0b4b118478961edb4c6b95fdf1f5ebc79f79
Bishoy <bishoy.samwuel@gmail.com>
2022-03-02 17:04:12 +0200

    Add currentId useState, Form , Posts components

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, AppBar, Typography, Grow, Grid, Box } from '@mui/material';
 import memories from './images/memories.png'
 import Posts from "./components/Posts/Posts";
@@ -9,8 +9,8 @@ import { getPosts } from "./Redux/posts";
 
 const App = () => {
   const dispactch = useDispatch();
-
-  useEffect(()=>{
+  const [currentId, setCurrentId] = useState(null);
+  useEffect(() => {
     dispactch(getPosts())
   }, [dispactch])
   return (
@@ -19,16 +19,16 @@ const App = () => {
         <Typography sx={styles.heading} variant="h2" align="center">
           Memories
         </Typography>
-        <Box component="img" sx={styles.image} alt="memories"  src={memories} />
+        <Box component="img" sx={styles.image} alt="memories" src={memories} />
       </AppBar>
       <Grow in>
         <Container>
           <Grid container justify="space-between" alignItems="stretch" spacing={3}>
             <Grid item xs={12} sm={7} >
-              <Posts />
+              <Posts setCurrentId={setCurrentId }/>
             </Grid>
             <Grid item xs={12} sm={4}>
-              <Form />
+              <Form currentId={currentId} />
             </Grid>
           </Grid>
         </Container>
